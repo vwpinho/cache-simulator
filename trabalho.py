6b756ae8f84dc2009e26395893c8d8a18d7c8017
Vitor Pinho <vwdpinho@inf.ufpel.edu.br>
2019-05-29 18:24:24 -0300

    Remocao de comentarios

--- trabalho.py
+++ trabalho.py
@@ -1,68 +1,44 @@
+'''
+Arguments are read as strings
+The first arg is the name of the program argv[0]
+The second arg is the configurations separated by : argv[1]
+The third arg is the name of the binary file with the addresses argv[2]
+
+Cache addressed by byte
+Addresses with 32 bits
+'''
+# imports
 from cache import Cache
-
-'''
-Argumentos sao lidos como string
-Da maneira do professor teremos como primeira entrada o nome do programa argv[0]
-Como segunda entrada as configuracoes separadas por : argv[1]
-E na terceira o nome do arquivo de acessos argv[2]
-A cada 8 bytes um inteiro e escrito
-arq.read(1) le 1 byte
-'''
 import sys
-import struct
-import array
-import math
 
-''' 
-Testes
-print(len(sys.argv))
-print(type(sys.argv[2]))
-print(sys.argv[1].split(':')) 
-'''
-'''
-    Cache enderecada a byte
-    End de 32 bits
-
-
-
-'''
-#print(len(sys.argv))
+# read arguments
 if len(sys.argv) == 2:
     config = [256, 4, 1, 'r']
     inFile = sys.argv[1]
 else:
     config = sys.argv[1].split(':')
     inFile = sys.argv[2]
-acessos = []
-#print(config)
-#print(inFile)
-# arq = open('teste.dat', 'wb')
-#  *************  FUNCIONA POREM GAMBIARRA  ***********
-arq = open(inFile, 'rb')
-'''b = arq.read(4)
-#s = str(bin(int(struct.unpack('I', b)[0] / 16777216)))[2:]
-#print(s)
 
-while b:
-    #   print(struct.unpack('I', b)[0] / 16777216)
-    s = bin((int(struct.unpack('I', b)[0] / 16777216)))[2:]
-    acessos.append(s)
-    b = arq.read(4)
-#print(type(acessos[1]))
-#print(len(acessos))
-#ind = int(str(acessos[1])[10:20], 2)
-#print(ind)'''
+# read the binary file
+arq = open(inFile, 'rb')
 b = arq.readlines()
 acessos = str(b[0]).split('0b')[1:]
+
+# cache init with the params
 c = Cache(int(config[0]), int(config[1]), int(config[2]), config[3])
+
+# simulation
 for i in range(len(acessos)):
     if i == len(acessos)-1:
         c.write(acessos[i][:len(acessos[i])-1])
     else:
         c.write(acessos[i])
+
+# simulation results
 print('MissComp {}'.format(c.missComp))
 print('MissConf {}'.format(c.missConf))
 print('MissCap {}'.format(c.missCap))
 print('Hits {}'.format(c.hits))
 
+# closing arq
 arq.close()
