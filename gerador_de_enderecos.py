1e5676cdb29acc975cd6a362c1575c8ad04c0c8c
Vitor Pinho <vwdpinho@inf.ufpel.edu.br>
2019-05-29 19:31:45 -0300

    Ajustes gerador de end

--- gerador_de_enderecos.py
+++ gerador_de_enderecos.py
@@ -2,13 +2,12 @@ import random
 import sys
 import struct
 params = sys.argv[1].split(':')
-print(params)
 numEnd = int(params[0])
 valMax = int(params[1])
 arqName = sys.argv[2]
 arq = open(arqName, 'wb')
 for i in range(int(numEnd)):
     j = random.randint(0, int(valMax))
-    print(j)
     arq.write(struct.pack('I', j))
+print('Enderecos gerados com sucesso!')
 arq.close()
